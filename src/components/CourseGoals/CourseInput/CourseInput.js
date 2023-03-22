ecb9d51c8d776db00275b70784063e873663fb83
Mr-Psmith <psancthus@gmail.com>
2023-03-22 16:35:16 -0100

    Worked on dynamic styling a lot

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      /* trim is for ruling out blanks(trims excess blanks) and this line means that we can't add empty in the app*/
+      setIsValid(false);
+      return; /* It stops when you return, so won't execute nothing if it is blank */
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? "invalid" : "" }`}>  {/* as the className is dynamic we can add and take styles from/to it; ``Because we can inject a dynamic value in this with  and this can be any js expression} */}
+        <label /* style={{ color: !isValid ? "red" : "black" }} */>Course Goal</label>
+        <input
+          /* style={{
+            border: !isValid ? "dotted 1px red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text" */
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
@@ -27,3 +43,4 @@ const CourseInput = props => {
 };
 
 export default CourseInput;
+
